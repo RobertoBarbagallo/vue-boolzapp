343f239b39515d6b066d50a1019ff9666e63e1c4
RobertoBarbagallo <roberto-barbagallo@live.it>
2021-05-05 13:10:57 +0200

    modifiche viste in calsse

--- scripts/app.js
+++ scripts/app.js
@@ -3,11 +3,34 @@ const app = new Vue({
     data: {
         usersList: globalUsersList,
         clickedUser: "",
+       
+    },
+
+    computed: {
+        getLastAccess(){
+            if(!this.clickedUser.messages){
+                return ""
+            }
+            const recievedMsgs = this.clickedUser.messages.filter(msgs => msgs.status === "received")
+
+
+            const lastAccess = recievedMsgs[recievedMsgs.length -1].date
+
+            return this.formatTime(lastAccess)
+        }
     },
 
     methods: {
         onUserClick: function(myUser){
            return this.clickedUser = myUser
         },
+
+        formatTime: function (date){
+            return moment(date, "DD/MM/YYYY HH:mm:ss").format("HH:mm")
+        },
+    },
+
+    mounted (){
+        this.clickedUser = this.usersList[0]
     }
 })
